141478440cf390cd14ab7f23cc906449ea43f14e
Robert Riemens <rriemens@my.centennialcollege.ca>
2016-11-22 09:40:09 -0500

    Add files via upload
    Ok make a database called GameProfile and run this query and you have the basic sql

--- GameSitetestsql.sql
+++ GameSitetestsql.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE GameSite;
 CREATE TABLE dbo.Member(
-   MemberID INT NOT NULL,
+   MemberID INT IDENTITY (100, 1) NOT NULL,
    Fname VARCHAR (20) NOT NULL,
    Lname VARCHAR (20) NOT NULL,
    Username VARCHAR (20) NOT NULL,
@@ -8,21 +8,22 @@ CREATE TABLE dbo.Member(
    PRIMARY KEY( MemberID )
 );
 CREATE TABLE dbo.Game(
-   GameID INT NOT NULL,
+   GameID INT IDENTITY (100, 1) NOT NULL,
+   GameName VARCHAR (20) NOT NULL,
+   Numberofusers INT DEFAULT 0,
    GDescription VARCHAR (50) ,
    Consoles VARCHAR (30) ,
    DatePublished DATE,
    PRIMARY KEY( GameID )
 );
 CREATE TABLE dbo.GameLine(
-   LinkID INT NOT NULL,
+   LinkID INT IDENTITY (100, 1)  NOT NULL,
    MemberID INT NOT NULL,
    GameID INT NOT NULL,
-   DateCreated DATE,
    PRIMARY KEY( LinkID )
 );
 ALTER TABLE dbo.GameLine
    ADD FOREIGN KEY (MemberID) REFERENCES dbo.Member (MemberID);
 
    ALTER TABLE dbo.GameLine
-   ADD FOREIGN KEY (GameID) REFERENCES dbo.Game (GameID);
+ADD FOREIGN KEY (GameID) REFERENCES dbo.Game (GameID);
